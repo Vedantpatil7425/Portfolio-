af1d0029bff917e2d1a51c4e4e4b7bd0b21936f4
Vedant Patil <167565378+Vedantpatil7425@users.noreply.github.com>
2024-08-09 23:22:18 +0530

    please check

---
+++ portfolio/main.js
@@ -0,0 +1,7 @@
+var typed= new typed (".text",{
+    Strings:["Frontend Devloper" , "Cybersecurity researcher" , "Ethical Hacker"],
+    typespeed: 100,
+    backspeed: 100,
+    backdelay: 1000,
+    loop: true
+})
